c27f78c7d0199449933761d8b88ea7625e6d7c64
Ben <benj08082008@icloud.com>
2026-02-06 10:56:01 -0800

    Pure Pursuit 50% implemented

--- src/PurePursuit.cpp
+++ src/PurePursuit.cpp
@@ -14,6 +14,8 @@ const int8_t PathLimit = 255;
 
 double Paths[255][1];
 
+double BestPoint[1]; 
+
 
 
 bool CheckForIntersection(double p1[1] , double p2[1])
@@ -68,4 +70,42 @@ bool CheckForIntersection(double p1[1] , double p2[1])
         return false;
     }
     
+}
+
+
+bool GetBestPoint()
+{
+    double Distance[1], x[1], y[1];
+    double p1[1], p2[1];
+
+    for (int i = PathLimit; i > 0; i--)
+    {
+        x[0] = Paths[i+1][0];
+        x[1] = Paths[i][0];
+        y[0] = Paths[i+1][1];
+        y[1] = Paths[i][1];
+
+        p1[0] = x[0];
+        p1[1] = y[0];
+        p2[0] = x[1];
+        p2[1] = y[1];
+
+        if (CheckForIntersection(p1,p2))
+        { // has intersected, checking best point (out of two)
+            Distance[0] = x[1] - Sol1[0];
+            if (abs(Sol1[0] - x[1]) < abs(Sol2[0] - x[1]) || abs(Sol1[1] - x[1]) < abs(Sol2[1] - x[1]))
+            {
+                Distance[1] = Sol1[1];
+                Distance[0] = Sol1[0];
+                BestPoint[0] = Distance[0];
+                BestPoint[1] = Distance[1];
+            }else
+            {
+                Distance[1] = Sol2[1];
+                Distance[0] = Sol2[0];
+                BestPoint[0] = Distance[0];
+                BestPoint[1] = Distance[1];
+            }
+        }
+    }
 }
